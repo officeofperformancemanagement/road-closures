f353c5814e26eed100bd0951aadea22874bf87c4
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2024-09-27 09:58:03 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,4 @@ curl "https://www.google.com/maps/d/kml?mid=1apjIf_aUSOhV-ONHZAHASx-TJxvuRyM&for
 ogr2ogr ./files/data.geojson ./files/data.kml
 
 # convert to csv
-ogr2ogr -dim XY -f CSV -lco GEOMETRY=AS_WKT -lco GEOMETRY_NAME=boundary -nlt MULTIPOLYGON -sql "SELECT Name AS name,description AS description FROM \"Closures\" WHERE OGR_GEOMETRY='MultiLine' or OGR_GEOMETRY='Line'" -skipfailures ./files/data.csv ./files/data.kml
+# ogr2ogr -dim XY -f CSV -lco GEOMETRY=AS_WKT -lco GEOMETRY_NAME=boundary -nlt MULTIPOLYGON -sql "SELECT Name AS name,description AS description FROM \"Closures\" WHERE OGR_GEOMETRY='MultiLine' or OGR_GEOMETRY='Line'" -skipfailures ./files/data.csv ./files/data.kml
